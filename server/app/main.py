e4f901f6e50fbca6171393861848f4287e40b0fa
Dibash Thapa <dibashthapa55@gmail.com>
2022-12-07 10:46:53 +0545

    initial commit

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,6 @@
-from .config import app, sio
 import uvicorn
 import logging
 import sys
-from .views import home
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
