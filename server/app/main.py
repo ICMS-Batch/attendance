e3e18c23edb82c130c5de592e8ebcf6f085e319d
Aryan Tamang <aryantamang@DESKTOP-MOP3UPK.localdomain>
2022-12-07 12:11:29 +0545

    Aryan Changes

--- server/app/main.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-import logging
-import sys
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
--- server/app/static/js/sockets.js
+++
@@ -1,4 +0,0 @@
-// var url = window.location.hostname;
-// var socket = io(url+':8000', {
-// path: '/ws/socket.io', transports:["websocket"]
-// });
