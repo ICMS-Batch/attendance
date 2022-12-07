4e62c6d7936a481749f323b9db3db76e3624ae07
Aryan Tamang <aryantamang@DESKTOP-MOP3UPK.localdomain>
2022-12-07 12:42:55 +0545

    Aryan Changes

--- server/apps/auth.py
+++ server/apps/auth.py
@@ -14,18 +14,44 @@ def signup() :
         data = request.data
         #the data is in bytes so we need to convert it into the string using decode functin
         main_data = json.loads((data.decode("UTF-8")))
-        #now loading the database
-        student = models.Students(main_data["firstname"].lower()
-                                ,main_data["lastname"].lower()
-                                ,main_data["email"].lower()
-                                ,main_data["password"])
-        
-        #now adding to the database
-        db.session.add(student)
-        db.session.commit()
+        #checking if email existed before or not
+        email = main_data["email"]
+        data_from_database = models.Students.query.filter_by(email = email).first()
 
-        
+        if (data_from_database == None) :
+            #now loading the database
+            student = models.Students(main_data["firstname"].lower()
+                                    ,main_data["lastname"].lower()
+                                    ,main_data["email"].lower()
+                                    ,main_data["password"])
+            
+            #now adding to the database
+            db.session.add(student)
+            db.session.commit()
 
+            return "Successfully Registered"
 
+        else :
+            return "Email already existed."
     return ""
 
+@auth.route("/api/login-data",methods=["GET","POST"])
+@cross_origin()
+def login() :
+    if request.method == "POST" :
+            data = request.data
+            #now converting the data into string and json
+            main_data = json.loads((data.decode("UTF-8")))
+            auth_email = main_data["email"]
+            auth_pass = main_data["password"]
+
+            #now fetching the data from the database
+            data_from_database = models.Students.query.filter_by(email = auth_email).first()
+            if (data_from_database != None) :
+                if(data_from_database.password == auth_pass):
+                    return "Login Successful"
+                else :
+                    return "Wrong Password"
+            else :
+                return "Wrong Email"
+    return ""
