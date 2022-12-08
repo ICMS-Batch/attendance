4d7710e9cfc211f7ca9cbab89122079d7f3eeaa6
Sanjay-Khadka <sanjay1khadka@gmail.com>
2022-12-08 10:52:51 +0545

    feat:added private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Router, Route, Routes } from "react-router-dom";
+
 import * as css from "./css/app.css";
-import HomeScreen from "./pages/HomeScreen";
+import Home from "./pages/Home";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <div>
-      <SignUp />
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 };
