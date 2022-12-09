37036388f78e421eac261a0b4ebbffb8e3f177a3
Dibash Thapa <dibashthapa55@gmail.com>
2022-12-09 13:21:02 +0545

    Docker file for server

--- server/apps/views.py
+++ server/apps/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 #defining the blueprint
 views = Blueprint("views",__name__)
 
-@views.route("/")
+@views.route("")
 def index():
     return "One piece is real"
--- server/server.py
+++
@@ -1,6 +0,0 @@
-from apps import create_app
-
-#defining the main function
-if __name__ == "__main__" :
-    #running the application
-    create_app()
