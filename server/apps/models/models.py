2e845406070259c8673ee948391ede72fbfa2234
Aryan Tamang <aryantamang@DESKTOP-MOP3UPK.localdomain>
2022-12-09 11:12:30 +0545

    Aryan Changes

--- server/apps/models/models.py
+++ server/apps/models/models.py
@@ -17,3 +17,19 @@ class Students(db.Model,SerializerMixin):
         self.last_name = last_name
         self.email = email
         self.password = password
+
+#defining the table
+class Admin(db.Model,SerializerMixin) :
+
+    #definng the attributes
+    first_name = db.Column("First Name",db.String(100))
+    last_name = db.Column("Last Name",db.String(100))
+    email = db.Column("Email",db.String(200))
+    password = db.Column("Password",db.String(100))
+
+     #defining the constructor
+    def __init__(self,first_name,last_name,email,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
