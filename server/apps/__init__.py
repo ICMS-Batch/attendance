4581717375d5d665e32099a2eb9d0ab2762da73b
Dibash Thapa <dibashthapa55@gmail.com>
2022-12-10 16:43:41 +0545

    configure github actions and tests

--- server/apps/__init__.py
+++ server/apps/__init__.py
@@ -17,14 +17,9 @@ def create_app(env="apps.config.EnvironmentConfig") -> Flask:
     """
     app = Flask(__name__)
     app.config.from_object(env)
-    print(env)
-    print(app.config["SQLALCHEMY_DATABASE_URI"])
     db.init_app(app)
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth)
-
-    # with app.app_context():
-    #     db.create_all()
 
     return app
